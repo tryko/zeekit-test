89a6d4d36bd0cb9eb4ba0acc211bd61674b8857f
itay <itay83ko@gmail.com>
2017-03-12 22:21:25 +0200

    zeekit-test

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <p>adfafd</p>
+    {{showMovies}}
+  </div>
+</template>
+<script>
+
+  export default {
+    name: 'app',
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+      showMovies() {
+        return this.$store.state.movieList
+      }
+    }
+  }
+</script>
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  
+  h1,
+  h2 {
+    font-weight: normal;
+  }
+  
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  
+  a {
+    color: #42b983;
+  }
+</style>
